d438daa2f6f52051e2e3bb2350a5ae6f4ef95bb1
rishabkm <119279619+rishabkm@users.noreply.github.com>
2025-10-18 23:34:03 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Employee{
+    public:
+    string name;
+    float salary;
+
+    void work(){
+        cout<<name<<" is working"<<endl;
+    }
+};
